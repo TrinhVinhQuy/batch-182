a8f519a49dec0c6e16e9d6e7f7a1a9fb7b5f5257
hnlong2000 <135562091+hnlong2000@users.noreply.github.com>
2023-10-10 09:38:01 +0700

    Update product.js

--- js/product.js
+++ js/product.js
@@ -10,15 +10,47 @@ $(document).ready(function () {
   });
 });
 
-var acc = document.querySelectorAll("#open__subnav");
-var i;
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        var subnav = this.nextElementSibling;
-      if (subnav.style.display === "block") {
-        subnav.style.display = "none";
-      } else {
-        subnav.style.display = "block";
+ var xhr = new XMLHttpRequest();
+
+  // Đường dẫn tới file home.html
+  var url = '/home.html';
+
+  // Mở kết nối và gửi yêu cầu GET để tải nội dung của file home.html
+  xhr.open('GET', url, true);
+  xhr.send();
+
+  // Xử lý khi yêu cầu đã được hoàn thành
+  xhr.onload = function() {
+  if (xhr.status === 200) {
+      // Lấy nội dung của file home.html từ response
+      var responseHTML = xhr.responseText;
+
+      // Tạo một temporary element để chứa nội dung
+      var tempElement = document.createElement('header');
+      tempElement.innerHTML = responseHTML;
+
+      // Tìm đối tượng có class name là "block-header"
+      var blockHeader = tempElement.querySelector('.block-header');
+
+      // Kiểm tra xem đối tượng tồn tại trước khi chèn vào file product.html
+      if (blockHeader) {
+      // Chèn đối tượng vào vị trí mong muốn trong file product.html
+      var headerContainer = document.getElementById('block-header-container');
+      headerContainer.appendChild(blockHeader);
       }
-    });
+
+      // Tạo một temporary element để chứa nội dung
+      var tempElement = document.createElement('footer');
+      tempElement.innerHTML = responseHTML;
+
+      // Tìm đối tượng có class name là "block-header"
+      var blockFooter = tempElement.querySelector('.block-footer');
+
+      // Kiểm tra xem đối tượng tồn tại trước khi chèn vào file product.html
+      if (blockFooter) {
+      // Chèn đối tượng vào vị trí mong muốn trong file product.html
+      var headerContainer = document.getElementById('block-footer-container');
+      headerContainer.appendChild(blockFooter);
+      }
   }
+  };
